aa9695143a0ce38b079b0a4175e505eb80e4321d
Andreas Busold <an.bu@gmx.net>
2013-06-16 14:22:43 +0200

    generate html output with unit and separator content tags

--- lib/countdown/counters/counter.rb
+++ lib/countdown/counters/counter.rb
@@ -4,23 +4,41 @@ module Countdown
     class Counter
       include ::Countdown::ContentTags
 
-      DEFAULT_DIRECTION = :down
-      DEFAULT_UNITS     = [:days, :hours, :minutes, :seconds]
+      DEFAULT_DIRECTION  = :down
+      DEFAULT_UNITS      = [:days, :hours, :minutes, :seconds]
+      DEFAULT_SEPARATORS = { years: {value: "Y"}, months: {value: "M"}, weeks: {value: "w"}, days: {value: "d"}, hours: {value: "h"}, minutes: {value: "m"}, seconds: {value: "s"}, millis: {value: "ms"} }
 
-      attr_reader :direction, :units, :time
+      attr_reader :direction, :units, :separators, :time
 
       def initialize(time, options)
-        @direction = options.delete(:direction) || DEFAULT_DIRECTION
-        @units     = options.delete(:units) || DEFAULT_UNITS
-        @time      = time
+        @direction  = options.delete(:direction) || DEFAULT_DIRECTION
+        @units      = options.delete(:units) || DEFAULT_UNITS
+        @separators = options.delete(:separators) || DEFAULT_SEPARATORS
+        @time       = time
       end
 
       def to_html
         CountdownTag.new(direction).to_s do
-          time.to_s
+          units.map do |unit|
+            html = ""
+
+            separator_tag = ContentTag.new(:span, class: "#{unit}-separator").to_s do
+              separators[unit][:value]
+            end
+
+            unit_tag = ContentTag.new(:span, class: "#{unit} #{unit}-#{time_to_unit(unit)}").to_s do
+              time_to_unit(unit)
+            end
+
+            html << separator_tag
+            html << unit_tag
+          end.join
         end
       end
 
+      def time_to_unit(unit)
+        1.to_s
+      end
     end
 
   end
--- test/countdown/view_helpers_test.rb
+++ test/countdown/view_helpers_test.rb
@@ -11,12 +11,14 @@ module Countdown
 
       it 'creates a default 24h countdown' do
         time = DateTime.now+1
-        assert_equal "<div class=\"countdown\">#{time}</div>", @view.countdown(time)
+        expected = "<div class=\"countdown\"><span class=\"days-separator\">d</span><span class=\"days days-1\">1</span><span class=\"hours-separator\">h</span><span class=\"hours hours-1\">1</span><span class=\"minutes-separator\">m</span><span class=\"minutes minutes-1\">1</span><span class=\"seconds-separator\">s</span><span class=\"seconds seconds-1\">1</span></div>"
+        assert_equal expected, @view.countdown(time)
       end
 
       it 'creates a default 24h countup' do
         time = DateTime.now+1
-        assert_equal "<div class=\"countup\">#{time}</div>", @view.countup(time)
+        expected = "<div class=\"countup\"><span class=\"days-separator\">d</span><span class=\"days days-1\">1</span><span class=\"hours-separator\">h</span><span class=\"hours hours-1\">1</span><span class=\"minutes-separator\">m</span><span class=\"minutes minutes-1\">1</span><span class=\"seconds-separator\">s</span><span class=\"seconds seconds-1\">1</span></div>"
+        assert_equal expected, @view.countup(time)
       end
 
     end
