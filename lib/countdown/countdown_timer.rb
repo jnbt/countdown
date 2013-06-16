14217e4391debeb04c82d1bef7946c8433880417
Andreas Busold <an.bu@gmx.net>
2013-06-16 22:53:14 +0200

    calculate duration within time units

--- lib/countdown/countdown_timer.rb
+++ lib/countdown/countdown_timer.rb
@@ -1,55 +1,46 @@
+require 'date'
+
 module Countdown
   class CountdownTimer
-    attr_reader :time, :years, :months, :weeks, :days, :hours, :minutes, :seconds, :millis
+    attr_reader :time, :duration_in_ms, :years, :months, :weeks, :days, :hours, :minutes, :seconds, :millis
 
     def initialize(time)
-      @time    = time
-      @years   = calculate_years
-      @months  = calculate_months
-      @weeks   = calculate_weeks
-      @days    = calculate_days
-      @hours   = calculate_hours
-      @minutes = calculate_minutes
-      @seconds = calculate_seconds
-      @millis  = calculate_millis
+      @time           = time
+      @duration_in_ms = duration_in_ms
+      @years          = duration[:years]
+      @months         = duration[:months]
+      @weeks          = duration[:weeks]
+      @days           = duration[:days]
+      @hours          = duration[:hours]
+      @minutes        = duration[:minutes]
+      @seconds        = duration[:seconds]
+      @millis         = duration[:millis]
     end
 
     def [](unit)
       send unit
     end
 
+    def duration_in_ms
+      ((time.to_time - DateTime.now.to_time).to_f.round*1000).to_i
+    end
+
     private
 
-    def calculate_years
-      1
+    def duration
+      @__duration ||= calculate_units
     end
 
-    def calculate_months
-      1
-    end
+    def calculate_units
+      rest, millis  = duration_in_ms.divmod(1000)
+      rest, seconds = rest.divmod(60)
+      rest, minutes = rest.divmod(60)
+      rest, hours   = rest.divmod(24)
+      rest, days    = rest.divmod(30) # TODO: month can be 28,29,31 aswell
+      years, months = rest.divmod(12)
+      weeks, days   = days.divmod(7)
 
-    def calculate_weeks
-      1
-    end
-
-    def calculate_days
-      1
-    end
-
-    def calculate_hours
-      1
-    end
-
-    def calculate_minutes
-      1
-    end
-
-    def calculate_seconds
-      1
-    end
-
-    def calculate_millis
-      1
+      {years: years, months: months, weeks: weeks, days: days, hours: hours, minutes: minutes, seconds: seconds, millis: millis}
     end
 
   end
--- test/countdown/countdown_timer_test.rb
+++ test/countdown/countdown_timer_test.rb
@@ -5,8 +5,14 @@ module Countdown
   class CountdownTimerTest < TestCase
 
     before do
-      time = DateTime.now+1
-      @timer = CountdownTimer.new(time)
+      now = DateTime.now
+      h12m30 = now + 413 + (1.0/24)*12 + (1.0/24/2)
+
+      @timer = CountdownTimer.new(h12m30)
+    end
+
+    it 'should calculate duration in ms' do
+      assert_equal 35728200000, @timer.duration_in_ms
     end
 
     it 'should calculate years' do
@@ -18,27 +24,27 @@ module Countdown
     end
 
     it 'should calculate weeks' do
-      assert_equal 1, @timer[:weeks]
+      assert_equal 3, @timer[:weeks]
     end
 
     it 'should calculate days' do
-      assert_equal 1, @timer[:days]
+      assert_equal 2, @timer[:days]
     end
 
     it 'should calculate hours' do
-      assert_equal 1, @timer[:hours]
+      assert_equal 12, @timer[:hours]
     end
 
     it 'should calculate minutes' do
-      assert_equal 1, @timer[:minutes]
+      assert_equal 30, @timer[:minutes]
     end
 
     it 'should calculate seconds' do
-      assert_equal 1, @timer[:seconds]
+      assert_equal 0, @timer[:seconds]
     end
 
     it 'should calculate millis' do
-      assert_equal 1, @timer[:millis]
+      assert_equal 0, @timer[:millis]
     end
 
   end
