b5362fcecdb4cfbe865a802a8982e64f2d059737
Andreas Busold <ab@neopoly.de>
2013-06-17 17:15:22 +0200

    gather upcoming years

--- test/countdown/time_span_test.rb
+++ test/countdown/time_span_test.rb
@@ -17,6 +17,14 @@ module Countdown
       assert_equal [2012, 2013, 2014], time_span.upcoming_years
     end
 
+    it 'gathers days by upcoming months' do
+      starting_time = DateTime.parse("2013-06-01 00:00:00")
+      target_time   = DateTime.parse("2013-12-01 00:00:00")
+      time_span     = TimeSpan.new(starting_time, target_time)
+
+      assert_equal [30, 31, 30, 31, 30, 31], time_span.days_by_upcoming_months
+    end
+
     describe 'duration in millis' do
 
       it 'should calculate duration for 1 day in the future' do
