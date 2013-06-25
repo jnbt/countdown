364fe46bc9cfc3c97a0a570fc68306052b112b14
Andreas Busold <ab@neopoly.de>
2013-06-25 20:24:24 +0200

    test duration chaining

--- test/time_spanner/time_units/time_unit_collection_test.rb
+++ test/time_spanner/time_units/time_unit_collection_test.rb
@@ -29,28 +29,6 @@ module TimeSpanner
         assert collection.units.last.is_a?(Minute)
       end
 
-      describe 'calculation by given units' do
-
-        it 'calculates 1 unit' do
-          starting_time = DateTime.parse('2013-04-03 00:00:00')
-          target_time   = DateTime.parse('2013-04-03 02:00:00')
-          collection = TimeUnitCollection.new(starting_time, target_time, [:hours])
-
-          assert_equal 2, collection.units.first.amount
-        end
-
-        it 'calculates 2 units in a row' do
-          starting_time = DateTime.parse('2013-04-03 00:00:00')
-          target_time   = DateTime.parse('2013-04-03 02:45:00')
-
-          collection = TimeUnitCollection.new(starting_time, target_time, [:hours, :minutes])
-
-          assert_equal 2, collection.units.first.amount
-          assert_equal 45, collection.units.last.amount
-        end
-
-      end
-
     end
   end
 end
