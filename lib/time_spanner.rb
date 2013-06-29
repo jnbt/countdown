26b47750d7b2e6dc43506351dcaebb4553b5b515
Andreas Busold <an.bu@gmx.net>
2013-06-29 21:04:04 +0200

    check if 'to_time' responds with a Time object

--- lib/time_spanner.rb
+++ lib/time_spanner.rb
@@ -6,9 +6,9 @@ require 'time_spanner/time_span_builder'
 module TimeSpanner
 
   def self.new(from, to, options={})
-    raise InvalidClassError, "Must be a Time object!" unless [from, to].all?{ |time| time.is_a?(Time)}
+    raise InvalidClassError, "Must convert to Time object!" unless [from, to].all?{ |obj| obj.respond_to?(:to_time) && obj.to_time.is_a?(Time)}
 
-    TimeSpanBuilder.new(from, to, options[:units])
+    TimeSpanBuilder.new from.to_time, to.to_time, options[:units]
   end
 
   class InvalidClassError < StandardError; end
--- test/time_spanner/time_spanner_test.rb
+++ test/time_spanner/time_spanner_test.rb
@@ -3,20 +3,22 @@ require 'test_helper'
 module TimeSpanner
   class TimeSpannerTest < TestCase
 
-    it 'should create a builder instance' do
+    it 'creates a builder instance' do
       from    = Time.now
-      to      = from+1
+      to      = from + 1
       options = {}
       builder = TimeSpanner.new(from, to, options)
 
       assert builder.is_a?(TimeSpanBuilder)
     end
 
-    it 'should ensure Time class' do
-      [Date, DateTime].each do |time|
-        assert_raises InvalidClassError do
-          TimeSpanner.new(DateTime.now, DateTime.now)
-        end
+    it 'ensures converting to Time' do
+      InvalidClass = Class.new do
+        def to_time; false end
+      end
+
+      assert_raises InvalidClassError do
+        TimeSpanner.new(InvalidClass.new, InvalidClass.new)
       end
     end
 
